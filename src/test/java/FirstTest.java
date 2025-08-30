9843d8d3e9115e5ca1166e524246d14f08219f77
carrieyanzh <carrieyanzh@example.com>
2025-08-30 17:10:46 +0800

    update pom.xml

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.Calculator;
@@ -5,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+
+
 
 @Tag("fast")
 class FirstTest {
@@ -16,5 +19,22 @@ class FirstTest {
     assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
     assertEquals("My 1st JUnit 5 test! 😎", testInfo.getDisplayName(), () -> "TestInfo is injected correctly");
   }
+
+  @Test
+  @DisplayName("My 1st JUnit 5 testAddition! 😎")
+  void testAddition() {
+    Calculator calculator = new Calculator();
+    assertAll("Calculator tests",
+      () -> {
+        System.out.println("Testing 1 + 1");
+        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+      },
+      () -> {
+        System.out.println("Testing 2 + 3");
+        assertEquals(5, calculator.add(2, 3), "2 + 3 should equal 5");
+      }
+    );
+  }
+
 
 }
